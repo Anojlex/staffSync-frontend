61ec3d5e6651e85c6b4666d32f7bedb36162e1dc
Anoj <anojkjayan1998@gmail.com>
2024-02-04 14:47:30 +0530

    axios

--- src/components/Utils/axiosInstance.jsx
+++ src/components/Utils/axiosInstance.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'https://staffsync-hrm.onrender.com/api/v1', // Use http or https based on your server setup
+    baseURL: 'https://staffsync.onrender.com/api/v1', // Use http or https based on your server setup
 
     headers: {
         'Content-Type': 'application/json',
