4235bcc7f9caac1b3681140135a92be1fe7b7437
Anoj <anojkjayan1998@gmail.com>
2024-02-10 14:09:45 +0530

    responsive

--- src/components/connect/ConnectContainer.jsx
+++ src/components/connect/ConnectContainer.jsx
@@ -18,7 +18,7 @@ const ConnectContainer = () => {
 
     return (
         <div className=' md: bg-[#e6e4ef] h-screen sm:ml-60 rounded-sm flex justify-center items-center p-8 mt-6 pt-12'>
-            <div className='flex justify-center items-center  w-[60%] h-[80%] bg-white rounded-lg '>
+            <div className='flex justify-center items-center w-[100%] h-[50%] sm:w-[60%] sm:h-[80%] bg-white rounded-lg '>
                 <form onSubmit={handleSubmit} className='flex flex-col w-1/2 ' >
                     <div className='flex justify-center m-2 font-bold text-gray-700'><h1>Connect With Team</h1></div>
                     <input onChange={e => setRoomId(e.target.value)} type="text" placeholder="Enter room ID" className='p-2 m-3 border-2 border-gray-300 rounded-md outline-none' />
